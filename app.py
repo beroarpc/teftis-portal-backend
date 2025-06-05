96931f6014a2d04b0c8391c0c6641d7f43647ce9
Berat Arpacı <beroarpc@gmmail.com>
2025-06-05 20:36:29 +0300

    JWT sistemi eklendi

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-# app.py
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 
 app = Flask(__name__)
-CORS(app) 
-
-@app.route('/')
-def home():
-    return "Teftiş Backend Çalışıyor"
+app.config['JWT_SECRET_KEY'] = 'gizli-anahtar-buraya'  
+jwt = JWTManager(app)
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
-    print("Gelen veri:", data)
 
     if username == "admin" and password == "1234":
-        return jsonify({"message": "Giriş başarılı"}), 200
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
     else:
         return jsonify({"message": "Geçersiz kullanıcı adı veya şifre"}), 401
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+@app.route('/dashboard-data', methods=['GET'])
+@jwt_required()
+def dashboard_data():
+    return jsonify({"message": "Dashboard verisi"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
