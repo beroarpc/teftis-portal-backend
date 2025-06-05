0f4efdf7fc7b03bca880ab0140fd07aa8bf6c61b
Berat Arpacı <beroarpc@gmmail.com>
2025-06-05 13:12:54 +0300

    Render için app.run eklendi

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Teftiş Kurulu Backend API Çalışıyor!"
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username == "admin" and password == "1234":
+        return jsonify({"message": "Giriş başarılı"}), 200
+    else:
+        return jsonify({"message": "Geçersiz kullanıcı adı veya şifre"}), 401
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
