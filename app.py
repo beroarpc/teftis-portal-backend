6b8c776966c562b18dd6353c3323f6fed2f4bd54
Berat Arpacı <beroarpc@gmmail.com>
2025-06-05 21:29:32 +0300

    JWT dekoratörü hatası düzeltildi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'gizli-anahtar-buraya'  
+app.config['JWT_SECRET_KEY'] = 'gizli-anahtar-buraya'
 jwt = JWTManager(app)
 
 @app.route('/login', methods=['POST'])
@@ -18,12 +18,10 @@ def login():
         return jsonify({"message": "Geçersiz kullanıcı adı veya şifre"}), 401
 
 @app.route('/dashboard-data', methods=['GET'])
+@jwt_required()
 def dashboard_data():
     return jsonify({
         "karsilama": "Hoş geldiniz, sayın başkanım.",
         "denetim_sayisi": 12,
         "aktif_soruşturma": 4
     })
-@jwt_required()
-
-
