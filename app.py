64900a3839f6546a3a318723126c51de46f53c77
Berat Arpacı <beroarpc@gmmail.com>
2025-06-07 18:39:14 +0300

    JWT ve dashboard endpoint güncellendi

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'gizli-anahtar-buraya'
-CORS(app)
+CORS(app, supports_credentials=True)  
+
 jwt = JWTManager(app)
 
+
+users = {
+    "admin": {"password": "1234", "rol": "başkan"},
+    "mufettis": {"password": "1234", "rol": "müfettiş"},
+    "mufettis_yardimcisi": {"password": "1234", "rol": "müfettiş yardımcısı"}
+}
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
-
-
-    users = {
-        "admin": {"password": "1234", "rol": "başkan"},
-        "mufettis": {"password": "1234", "rol": "müfettiş"},
-        "mufettis_yardimcisi": {"password": "1234", "rol": "müfettiş yardımcısı"}
-    }
 
     user = users.get(username)
 
@@ -31,14 +31,21 @@ def login():
     else:
         return jsonify({"message": "Geçersiz kullanıcı adı veya şifre"}), 401
 
-
 @app.route('/dashboard-data', methods=['GET'])
 @jwt_required()
 def dashboard_data():
-    current_user = get_jwt_identity()
-    return jsonify({
-        "karsilama": f"Hoş geldiniz, sayın {current_user['rol']}.",
-        "denetim_sayisi": 12,
-        "aktif_soruşturma": 4,
-        "rol": current_user['rol']
-    })
+    try:
+        current_user = get_jwt_identity()
+        print("Aktif kullanıcı:", current_user)  
+        return jsonify({
+            "karsilama": f"Hoş geldiniz, sayın {current_user['rol']}.",
+            "denetim_sayisi": 12,
+            "aktif_soruşturma": 4,
+            "rol": current_user['rol']
+        }), 200
+    except Exception as e:
+        print("Hata:", e)  # LOG: hatayı gör
+        return jsonify({"error": str(e)}), 401
+
+if __name__ == "__main__":
+    app.run(debug=True)
